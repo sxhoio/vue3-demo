800ed683782bd1b5e0ab4a6f75d67dd3685c2350
GuanM <30427262+sxhoio@users.noreply.github.com.>
2024-04-13 19:29:20 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+// main.ts
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(ElementPlus)
+app.mount('#app')
